00cf6aa4178c8067b473c4648444f68294266541
Faraz786 <bsef17m004@pucit.edu.pk>
2021-06-15 19:06:21 +0500

    updated

--- forms/hodeplagg_og/Harband.php
+++ forms/hodeplagg_og/Harband.php
@@ -29,6 +29,9 @@
                     <div class="form-group mb-4 mb-lg-6">
                         <textarea name="paragraph_text_Harband" id="paragraph_text_Harband" cols="50" rows="10" class="form-control border border-danger" placeholder="Please provide us with additional information about your needs." required></textarea>
                     </div>
+                    <div class="form-group mb-4 mb-lg-6 d-flex flex-row">
+                        <input class="form-control border border-danger col-6 d-flex justify-content-start" type="file" name="fileToUpload" id="fileToUpload" accept=".txt,.xlsx,.png,.jpg,.jpeg,.pdf,.pptx,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
+                    </div>
                     <div class="pt-4 mb-5">
                         <button type="submit" name="sendHarband" id="sendHarband" class="btn btn-lg py-1 btn-danger text-capitalize font-weight-bold" style="float:right;margin-top: -5%;">Send</button>
                     </div>
--- forms/hodeplagg_og/Russelue.php
+++ forms/hodeplagg_og/Russelue.php
@@ -26,6 +26,9 @@
                     <div class="form-group mb-4 mb-lg-6">
                         <textarea name="paragraph_text_Russelue" id="paragraph_text_Russelue" cols="50" rows="10" class="form-control border border-danger" placeholder="Please provide us with additional information about your needs." required></textarea>
                     </div>
+                    <div class="form-group mb-4 mb-lg-6 d-flex flex-row">
+                        <input class="form-control border border-danger col-6 d-flex justify-content-start" type="file" name="fileToUpload" id="fileToUpload" accept=".txt,.xlsx,.png,.jpg,.jpeg,.pdf,.pptx,.doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document">
+                    </div>
                     <div class="pt-4 mb-5">
                         <button type="submit" name="sendRusselue" id="sendRusselue" class="btn btn-lg py-1 btn-danger text-capitalize font-weight-bold" style="float:right;margin-top: -5%;">Send</button>
                     </div>
