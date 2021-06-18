171e359503569117e5ae5df7ce7bd71e6e41e3cb
Faraz786 <bsef17m004@pucit.edu.pk>
2021-06-18 17:47:55 +0500

    detail4

--- forms/submit/detail4.php
+++ forms/submit/detail4.php
@@ -9,5 +9,49 @@ if(!array_key_exists("clubloggedin", $_SESSION)){
 
 require_once("../../db.php");
 
+if(isset($_REQUEST["send"])){
+    $_SESSION["numberOfTotalRussekort"]= $_REQUEST["numberOfTotalRussekort"];
+    $_SESSION["numberOfDifferentRussekort"]= $_REQUEST["numberOfDifferentRussekort"];
+    $_SESSION["color"]= $_REQUEST["color"];
+    $_SESSION["description"]= $_REQUEST["description"];
+}
+else{
+    header("Location: ../../index.php"); /* Redirect browser */
+    exit;
+}
+$target_dir = "../../requestuploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$newname = "";
+$uploadOk = 1;
+$info = pathinfo($_FILES['fileToUpload']['name']);
+$ext = "";
+if($info["basename"]!=null){
+    $ext = $info['extension']; // get the extension of the file
+}
+$nameid = uniqid();
+$newname = $nameid.".".$ext; 
+$target = '../../requestuploads/'.$newname;
+move_uploaded_file( $_FILES['fileToUpload']['tmp_name'], $target);
+if(!$info["basename"]){
+  $newname = null;    
+}
+$query_time = date("Y-m-d h:i:sa");
+$query = "INSERT INTO `club_requests`(`club_id`, `category_id`, `product_id`, `color`, `description`, `filename`, `created_on`) VALUES (\"{$_SESSION["userdata"]["club_id"]}\",\"{$_SESSION["cat_id"]}\",\"{$_SESSION["pro_id"]}\",\"{$_SESSION["color"]}\",\"{$_SESSION["description"]}\",\"$newname\",\"$query_time\")";
+db::getInstance()->dbquery($query);
+$query = "Select * from `club_requests` where `club_id` = \"{$_SESSION["userdata"]["club_id"]}\" and  `created_on` = \"{$query_time}\";";
 
+$result = db::getInstance()->get_result($query);
+if($result)
+{
+    $row = mysqli_fetch_assoc($result);
+    $query="INSERT INTO detail4(request_id,Number_of_Total_Russekort,Number_of_Different_Russekort) values (\"{$row["request_id"]}\",\"{$_SESSION["numberOfTotalRussekort"]}\",\"{$_SESSION["numberOfDifferentRussekort"]}\");";
+    db::getInstance()->dbquery($query);
+    header("Location: ../../userhome.php");
+    exit();  
+}
+else{
+    echo("Something went wrong. Try again later.");
+}
+exit();
 ?>
